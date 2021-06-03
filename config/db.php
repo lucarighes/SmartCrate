13a8a8de98ae5d937c3f831970dbe03970465b3b
luca.righes <luca.righes@gmail.com>
2021-06-03 14:54:54 +0200

    credentials

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=crate',
-    'username' => 'admin',
-    'password' => 'admin',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
